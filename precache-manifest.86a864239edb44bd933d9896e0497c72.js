f1f4ae30bb227fe611366374c98e2f2b24942447
minhanh <anminhoang@gmail.com>
2019-05-26 19:08:10 +0700

    Updates

---
+++ precache-manifest.86a864239edb44bd933d9896e0497c72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "605495cf6c77991c95e5d7e5aaa5474e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a71dc58711dc3d1e3fc",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "555cef1d786015fcba01",
+    "url": "/static/js/2.6e7644f7.chunk.js"
+  },
+  {
+    "revision": "4a71dc58711dc3d1e3fc",
+    "url": "/static/js/main.4c08037b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
